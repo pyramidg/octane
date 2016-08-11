6dfa45a7ec4b070604214be2ca21f007e06e2cdd
studioplace <webstudioplace.com>
2016-08-11 12:54:26 -0400

    created routes with templates

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('default', {title: 'Home'});
+  res.render('default', {
+    title: 'Home',
+    classname: 'home',
+    users: ['Pyramid', 'Sherretta', 'Nala']
+  });
   // between {title: 'Home'} is where Modules (source code, Objects or Json) are merged with Views (templates rendering data);
 });
-
 app.get('/about', function(req, res) {
-  res.send('<h1>About</h1> me');
+  res.render('default', {
+    title: 'About Us',
+    classname: 'about'
+  });
 });
 
-app.get('/about/:name?', function(req, res) {
-  var name = req.params.name;
-  res.send('<p>My name is '+name+" until otherwise</p>");
+app.get('*', function(req, res) {
+  res.send('Bad Route');
 });
-
 
 var server = app.listen(3000, function() {
   console.log('Listening on port 3000');
